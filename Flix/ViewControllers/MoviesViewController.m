e9b67a1c06059437d09e857c1ed7c8c0fd6b7e2b
Zavien Sibilia <zavien@fb.com>
2021-06-25 15:13:02 -0700

    fix search bar not properly searching for movies

--- Flix/ViewControllers/MoviesViewController.m
+++ Flix/ViewControllers/MoviesViewController.m
@@ -31,13 +31,13 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
-    [self fetchMovies];
+    self.movieSearchBar.delegate = self;
     
     self.refreshControl = [[UIRefreshControl alloc] init];
     [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
     [self.tableView insertSubview:self.refreshControl atIndex:0];
     
-    self.movieSearchBar.delegate = self;
+    [self fetchMovies];
 }
 
 - (void)fetchMovies {
@@ -77,6 +77,44 @@
     [task resume];
 }
 
+- (void)fetchTrailers {
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+               UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Network Error" message:@"Failed to connect to the internet" preferredStyle:(UIAlertControllerStyleAlert)];
+               
+               // create an OK action
+               UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                                  style:UIAlertActionStyleDefault
+                                                                handler:^(UIAlertAction * _Nonnull action) {
+                                                                        // handle response here.
+                                                                }];
+               // add the OK action to the alert controller
+               [alert addAction:okAction];
+               
+               [self presentViewController:alert animated:YES completion:^{
+                   // optional code for what happens after the alert controller has finished presenting
+               }];
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               
+               self.movies = dataDictionary[@"results"];
+               self.filteredData = self.movies;
+
+               [self.tableView reloadData];
+               
+           }
+        [self.refreshControl endRefreshing];
+        [self.loadingIcon stopAnimating];
+       }];
+    [task resume];
+}
+
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return self.filteredData.count;
 }
@@ -100,7 +138,7 @@
     return cell;
 }
 
-- (void)movieSearchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
     
     
     if (searchText.length != 0) {
@@ -109,6 +147,7 @@
             NSDictionary *movie = (NSDictionary*) evaluatedObject;
             return [movie[@"title"] containsString:searchText];
         }];
+        
         self.filteredData = [self.movies filteredArrayUsingPredicate:predicate];
         
         NSLog(@"%@", self.movies);
